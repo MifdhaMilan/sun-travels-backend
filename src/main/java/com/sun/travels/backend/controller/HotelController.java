11ef3b6f5aba9066413e0ea0f552600b45fbc300
MifdhaMilan <mifmilan@gmail.com>
2022-08-14 19:49:55 +0530

    hotel

--- src/main/java/com/sun/travels/backend/controller/HotelController.java
+++ src/main/java/com/sun/travels/backend/controller/HotelController.java
@@ -1,14 +1,13 @@
 package com.sun.travels.backend.controller;
 
 import com.sun.travels.backend.model.Hotel;
-import com.sun.travels.backend.request.HotelRequest;
 import com.sun.travels.backend.service.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/hotel")
@@ -17,7 +16,24 @@ public class HotelController {
     private HotelService hotelService;
 
     @PostMapping("/create-hotel")
-    public ResponseEntity<Hotel> createHotel(@RequestBody HotelRequest hotelRequest){
-        return hotelService.createHotel(hotelRequest);
+    public ResponseEntity<Hotel> createHotel(@RequestParam String name, @RequestParam MultipartFile image, @RequestParam String no, @RequestParam String street,@RequestParam String city,@RequestParam String country){
+        return hotelService.createHotel(name, image, no, street, city, country);
+    }
+    @GetMapping("/get-hotel/{hid}")
+    public ResponseEntity<Hotel> getHotelById(@PathVariable int hid){
+        return hotelService.getHotelById(hid);
+    }
+    @GetMapping("/get-all-hotels")
+    public ResponseEntity<List<Hotel>> getAllHotels(){
+        return hotelService.getAllHotels();
+    }
+    @PutMapping("/update-hotel")
+    public String updateHotel(@RequestParam int hid,@RequestParam String name,@RequestParam MultipartFile image,@RequestParam String no,  @RequestParam String street,@RequestParam String city, @RequestParam String country){
+        return hotelService.updateHotel(hid, name, image, no, street, city, country);
+    }
+
+    @DeleteMapping("/delete-hotel/{hid}")
+    public void deleteHotel(@PathVariable int hid){
+        hotelService.deleteHotel(hid);
     }
 }
--- src/main/java/com/sun/travels/backend/service/HotelService.java
+++ src/main/java/com/sun/travels/backend/service/HotelService.java
@@ -4,10 +4,12 @@ import com.sun.travels.backend.model.Address;
 import com.sun.travels.backend.model.Hotel;
 import com.sun.travels.backend.repository.AddressRepository;
 import com.sun.travels.backend.repository.HotelRepository;
-import com.sun.travels.backend.request.HotelRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Service
 public class HotelService {
@@ -17,18 +19,59 @@ public class HotelService {
     @Autowired
     private AddressRepository addressRepository;
 
-    public ResponseEntity<Hotel> createHotel(HotelRequest hotelRequest){
+    public ResponseEntity<Hotel> createHotel(String name,MultipartFile image,String no,  String street,String city, String country){
         Address address = new Address();
-        address.setNo(hotelRequest.getAddressRequest().getNo());
-        address.setStreet(hotelRequest.getAddressRequest().getStreet());
-        address.setCity(hotelRequest.getAddressRequest().getCity());
-        address.setCountry(hotelRequest.getAddressRequest().getCountry());
+        address.setNo(no);
+        address.setStreet(street);
+        address.setCity(city);
+        address.setCountry(country);
         addressRepository.save(address);
 
         Hotel hotel = new Hotel();
-        hotel.setName(hotelRequest.getName());
-        hotel.setImage(hotelRequest.getImage());
+        hotel.setName(name);
+        try{
+            hotel.setImage(image.getBytes());
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
         hotel.setAddress(address);
         return ResponseEntity.ok(hotelRepository.save(hotel));
+    }
+    public ResponseEntity<Hotel> getHotelById(int hid){
+        return ResponseEntity.ok(hotelRepository.findById(hid).orElse(null));
+    }
+
+    public ResponseEntity<List<Hotel>> getAllHotels(){
+        return ResponseEntity.ok(hotelRepository.findAll());
+    }
+
+    public String updateHotel(int hid,String name,MultipartFile image,String no,  String street,String city, String country){
+        Hotel existingHotel = getHotelById(hid).getBody();
+        if(existingHotel != null){
+            try{
+                Address existingAddress = addressRepository.findById(existingHotel.getAddress().getAid()).orElse(null);
+                existingAddress.setNo(no);
+                existingAddress.setStreet(street);
+                existingAddress.setCity(city);
+                existingAddress.setCountry(country);
+                addressRepository.save(existingAddress);
+                existingHotel.setName(name);
+                existingHotel.setImage(image.getBytes());
+                existingHotel.setAddress(existingAddress);
+                hotelRepository.save(existingHotel);
+            }catch(Exception e){
+                System.out.println(e);
+            }
+            return "hotel updated successfully";
+
+        }else{
+            return "a hotel doesn't exist with this id";
+        }
+    }
+
+    public String deleteHotel(int hid){
+        hotelRepository.deleteById(hid);
+        return "hotel deleted successfully";
     }
 }
